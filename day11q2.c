d71a26409f55cfd04b4db780243894bb3466e7d0
arnika-18 <arnikasaini1810@gmail.com>
2026-09-13 14:53:16 -0400

    added day11 programs

---
+++ day11q2.c
@@ -0,0 +1,31 @@
+Write a program to find profit or loss percentage given cost price and selling price.
+
+#include <stdio.h>
+
+int main()
+{
+    float cp, sp, profit, loss, percentage;
+
+    scanf("%f %f", &cp, &sp);
+
+    if(sp > cp)
+    {
+        profit = sp - cp;
+        percentage = (profit / cp) * 100;
+
+        printf("Profit %.0f%%", percentage);
+    }
+    else if(sp < cp)
+    {
+        loss = cp - sp;
+        percentage = (loss / cp) * 100;
+
+        printf("Loss %.0f%%", percentage);
+    }
+    else
+    {
+        printf("No Profit No Loss");
+    }
+
+    return 0;
+}
