0451fbc51911b3d99f0490a4d1ab8e2c93704ca5
arnika-18 <arnikasaini1810@gmail.com>
2026-09-13 15:06:21 -0400

    added day29 programs

---
+++ day29q1.c
@@ -0,0 +1,22 @@
+Q57: Find the sum of array elements.
+
+#include <stdio.h>
+
+int main()
+{
+    int n, sum = 0;
+
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+        sum = sum + arr[i];
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
