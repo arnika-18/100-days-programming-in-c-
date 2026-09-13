a209d8002ca2ff552a590d87671205afc088bb6e
arnika-18 <arnikasaini1810@gmail.com>
2026-09-13 15:08:48 -0400

    added day33 programs

---
+++ day33q2.c
@@ -0,0 +1,36 @@
+Q66: Insert an element in a sorted array at the appropriate position.
+
+#include <stdio.h>
+
+int main()
+{
+    int n, element;
+
+    scanf("%d", &n);
+
+    int arr[n + 1];
+
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d", &element);
+
+    int i = n - 1;
+
+    while(i >= 0 && arr[i] > element)
+    {
+        arr[i + 1] = arr[i];
+        i--;
+    }
+
+    arr[i + 1] = element;
+
+    for(i = 0; i <= n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
