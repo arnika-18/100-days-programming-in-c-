7404c8c8fb33c81dcc76735f710c8e99a58a7d48
arnika-18 <arnikasaini1810@gmail.com>
2026-09-13 15:03:29 -0400

    added day26 programs

---
+++ day25q2.c
@@ -0,0 +1,28 @@
+Q50: Write a program to print the following pattern:
+*****
+ ****
+  ***
+   **
+    *
+#include <stdio.h>
+
+int main()
+{
+    for(int i = 1; i <= 5; i++)
+    {
+        for(int j = 1; j < i; j++)
+        {
+            printf(" ");
+        }
+
+        for(int j = 1; j <= 6 - i; j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
+
